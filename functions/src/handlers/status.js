40b90ba5319c8d7dfbffa464d8892ab84effa75d
Gilad Maoz <gilad.maoz@gmail.com>
2020-09-22 15:15:06 +0200

    inc. demo env file

--- functions/src/handlers/status.js
+++ functions/src/handlers/status.js
@@ -1,8 +1,6 @@
-exports.status = (req, res) => {
-    /**
-     * Get status (health check for FB)
-     * @param req
-     * @param res
-     */
-    return res.status(200).send("I'm alive!").end();
-};
+/**
+ * Get status (health check for FB)
+ * @param req
+ * @param res
+ */
+exports.status = (req, res) => res.status(200).send("I'm alive!").end();
